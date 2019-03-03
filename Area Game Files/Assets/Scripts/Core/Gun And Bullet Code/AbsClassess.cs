7da22527330715b1fefa11b6fad28eae2e40c719
Gonzako <al341522@uji.es>
2019-03-03 21:20:41 +0100

    Start on the take on Bullets and Guns
    Draft-code about the bullet and gun classes (WIP)

---
+++ Area Game Files/Assets/Scripts/Core/Gun And Bullet Code/AbsClassess.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbsGun : MonoBehaviour
+{
+    public AbsBullet GunBullet; //Bullet that will shoot 
+
+    //Stats for any kind of weapon
+    public float FireRate;
+    public float ReloadTime;
+    public int MaxAmmo;
+    public int CurrentAmmo;
+
+
+
+
+    // Start is called before the first frame update
+    public virtual void Start() { }
+
+    // Update is called once per frame
+    public virtual void Update(){ }
+}
+
+
+public abstract class AbsBullet : MonoBehaviour
+{
+
+
+
+
+}
