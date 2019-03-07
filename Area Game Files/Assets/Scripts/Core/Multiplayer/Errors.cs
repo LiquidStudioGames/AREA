9d250be9087697e2f3b5cc71dfc71d080829c14d
Daniel Munch <30357821+Dmunch04@users.noreply.github.com>
2019-03-07 10:24:10 +0100

    Added 2 error messages

--- Area Game Files/Assets/Scripts/Core/Multiplayer/Errors.cs
+++ Area Game Files/Assets/Scripts/Core/Multiplayer/Errors.cs
@@ -4,6 +4,8 @@ class Errors : MonoBehavior
 {
 
   Error_NameSpecify = "Please specify a name for the game!";
+  Error_ConnectToServer = "Failed to connect to server!";
+  Error_ConnectToGame = "Failed to connect to the game!";
   
   public static Errors Instance;
   
