f2a253896df392b3a3fa9a6b8a0cafb30574f488
Gonzako <al341522@uji.es>
2019-03-09 15:50:08 +0100

    Changed Player collision to Boxcolission and worked on PlayerMovement

--- Area Game Files/Assets/Scripts/Core/Multiplayer/Errors.cs
+++ Area Game Files/Assets/Scripts/Core/Multiplayer/Errors.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 class Errors : MonoBehaviour
 {
 
-  string Error_NameSpecify = "Please specify a name for the game!";
-  string Error_ConnectToServer = "Failed to connect to server!";
-  string Error_ConnectToGame = "Failed to connect to the game!";
-  
-  public static Errors Instance;
-  
-  void Awake ()
-  {
-    Instance = this;
-  }
+    // Sets the error messages to the errors
+    public string Error_NameSpecify = "Please specify a name for the game!";
+    public string Error_ConnectToServer = "Failed to connect to server!";
+    public string Error_ConnectToGame = "Failed to connect to the game!";
+    public string Error_CreateGame = "Failed to create the game!";
+    public string Error_JoinGame = "Failed to join the game!";
+
+    public static Errors Instance;
+
+    // Gets the status text element
+    public Text Status;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // SetErrorStatus Method :: Sets the status text to the error
+    public void SetErrorStatus(string Error)
+    {
+        Status.text = Error;
+    }
 
 }
