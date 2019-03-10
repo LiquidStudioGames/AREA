39154143e3d56caf9427a38aeacb4c9343192ac3
Daniel Munch <daniellmunch@gmail.com>
2019-03-10 21:03:04 +0100

    Removed DCF. Switched to Steam

---
+++ ModCreator/Assets/Scripts/SetFile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+public class SetFile : MonoBehaviour
+{
+
+    private Data data;
+
+    public void GetData (string FileName)
+    {
+
+        string path = Application.persistentDataPath + "/Gamemodes/" + FileName + ".json";
+
+        using (StreamReader file = File.OpenText(path))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            Data _data = (Data)serializer.Deserialize(file, typeof(Data));
+            Debug.Log(_data);
+        }
+
+    }
+
+    public void SetData (string Parent, string Value)
+    {
+
+        Debug.Log(data);
+
+    }
+
+}
