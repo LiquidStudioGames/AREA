596cabc911b198e7d7560870f3ff01f4c747a58c
Daniel Munch <daniellmunch@gmail.com>
2019-03-10 13:18:50 +0100

    Finished first part of login/register

---
+++ Area Game Files/Assets/Scripts/Core/UI/Menu/AccountUI.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AccountUI : MonoBehaviour
+{
+
+    // Takes in the username text
+    public Text Username;
+
+    // Crates a MenuStatus variable, called status
+    private MenuStatus Status;
+
+    void Awake ()
+    {
+
+        // Sets the status variable to an instance of menustatus
+        Status = MenuStatus.Instance;
+    }
+
+    // Start Method :: Gets called in the start
+    void Start ()
+    {
+
+        // Checks if the user is logged in
+        if (AccountManager.LoggedIn)
+        {
+
+            // If so, set the username text to the logged in username
+            Username.text = AccountManager.UserUsername;
+
+        }
+
+        // If not
+        else
+        {
+
+            // Then set the status text to the message
+            Status.SetStatus("You're not logged in");
+
+            // Why wouldn't you be logged in lmao?
+
+        }
+
+    }
+
+    // ButtonLogout Method :: Gets called when clicking on the logout menu button
+    public void ButtonLogout ()
+    {
+
+        // Checks if the user is logged in
+        if (AccountManager.LoggedIn)
+        {
+
+            // If you are, then call the logout method
+            AccountManager.Instance.Logout();
+
+        }
+
+        // If not
+        else
+        {
+
+            // Then set the status text to the message
+            Status.SetStatus("You're not logged in");
+
+            // Why wouldn't you be logged in lmao?
+
+        }
+
+    }
+
+}
---
+++ Area Game Files/Assets/Scripts/Core/UI/Menu/MenuStatus.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuStatus : MonoBehaviour
+{
+
+    // Creates an instance of this script
+    public static MenuStatus Instance;
+
+    // Awake Method :: Gets called in the very start
+    void Awake ()
+    {
+
+        // Sets the instance to this script
+        Instance = this;
+
+    }
+
+    // Takes in the status text
+    public Text Status;
+
+    // Start Method :: Gets called in the start
+    void Start ()
+    {
+
+        // Sets the status text to the message
+        Status.text = "Logged in. Connected. Ready!";
+
+    }
+
+    // SetStatus Method :: Sets the status text
+    public void SetStatus (string Text)
+    {
+
+        // Sets the status text to the given text
+        Status.text = Text;
+
+    }
+
+}
