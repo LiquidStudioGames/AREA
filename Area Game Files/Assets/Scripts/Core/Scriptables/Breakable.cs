fb6f1276f2786c22579eabbfb15f6a45539aa2b5
Paweł <pawlo168@gmail.com>
2019-03-11 19:07:39 +0100

    1
    1

--- Area Game Files/Assets/Scripts/Core/Scriptables/Breakable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptables/Breakable")]
-public class Breakable : ScriptableObject
-{
-  public int Health;          // Amount of health the object has
-  public Animation Animation; // Animation for when it breaks
-}
