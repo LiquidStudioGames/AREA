7c0e68c94dfc8a621f7b29ffb97482a887863954
Paweł <pawlo168@gmail.com>
2019-03-17 08:53:27 +0100

    After master merge

---
+++ Area Game Files/Assets/TestFilesW/bepuMyResolution/Scripts/DeliveryDamageGameType.cs
@@ -0,0 +1,153 @@
+﻿using DarkRift;
+using DarkRift.Client;
+using DarkRift.Client.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using TMPro;
+
+namespace Dim
+{
+    public class DeliveryDamageGameType : MonoBehaviour
+    {
+        float timeInfo;
+        bool run = false;
+        UnityClient uC;
+        ushort clientId;
+        public Text thisPlayerDamText, otherPlayerDamageText;
+        ushort thisPlayerDamage, otherPlayerDamage;
+        public Slider slider;
+        public string wonInfo = "You won", lostInfo = "You lost", passInfo = "Balance";
+        public Text infoText;
+        NetworkPlayerDimManager np;
+
+
+
+        private void FixedUpdate()
+        {
+            if (run)
+            {
+                timeInfo += Time.deltaTime;
+                infoText.text = timeInfo.ToString("F0");
+            }
+        }
+
+        public UnityClient Clientt
+        {
+            get
+            {
+                return uC;
+            }
+            set
+            {
+                uC = value;
+
+                np = FindObjectOfType<NetworkPlayerDimManager>();
+                uC.MessageReceived += MessageTimeRecive;
+                clientId = uC.ID;
+            }
+        }
+
+        void HidEndGameInfo()
+        {
+
+        }
+
+        void SetSlider()
+        {
+            if (thisPlayerDamage + otherPlayerDamage > 0)
+                slider.value = (float)((float)thisPlayerDamage / (float)(thisPlayerDamage + otherPlayerDamage));
+            else
+                slider.value = 0.5f;
+           // Debug.Log(thisPlayerDamage + "    " + otherPlayerDamage + "      " + slider.value);
+        }
+
+        void MessageTimeRecive(object sender, MessageReceivedEventArgs e)
+        {
+            using (Message message = e.GetMessage() as Message)
+            {
+                if (message.Tag == DimTag.DeliveryDamageInfo)
+                {
+                    using (DarkRiftReader reader = message.GetReader())
+                    {
+                        ushort index = reader.ReadUInt16();
+                        ushort damage = reader.ReadUInt16();
+                        ushort damageTaken = reader.ReadUInt16();
+
+
+                        if (index == clientId)
+                        {
+                            thisPlayerDamage = damage;
+                            thisPlayerDamText.text = damage.ToString();
+                        }
+                        else
+                        {
+                            otherPlayerDamage = damage;
+                            otherPlayerDamageText.text = damage.ToString();
+                        }
+                        SetSlider();
+
+                      //  Debug.Log(slider.value);
+
+                    }
+                    return;
+                }
+
+                if (message.Tag == DimTag.DeliveryDamageBreakStart)
+                {
+                    infoText.text = "";
+                    thisPlayerDamage = otherPlayerDamage = 0;
+                    timeInfo = 0;
+                    run = true;
+                    return;
+                }
+
+
+                if (message.Tag == DimTag.DeliveryDamageMatchStart)
+                {
+                    thisPlayerDamage = otherPlayerDamage = 0;
+                    timeInfo = 0;
+                    run = true;
+                    return;
+                }
+
+                if (message.Tag == DimTag.DeliveryDamageMatchEndStart)
+                {
+                    timeInfo = 0;
+                    run = false;
+
+                    if (np.GetPlayersCount() > 1)
+                    {
+                        if (thisPlayerDamage > otherPlayerDamage)
+                        {
+                            infoText.text = wonInfo;
+
+                        }
+                        else
+                        {
+                            if (otherPlayerDamage > thisPlayerDamage)
+                                infoText.text = lostInfo;
+                            else
+                                infoText.text = passInfo;
+
+                        }
+                    }
+                    else
+                        infoText.text = wonInfo;
+                    return;
+                }
+                //else if (message.Tag == Tags.SetRadiusTag)
+                //{
+                //    using (DarkRiftReader reader = message.GetReader())
+                //    {
+                //        ushort id = reader.ReadUInt16();
+
+                //        if (networkPlayers.ContainsKey(id))
+                //            networkPlayers[id].SetRadius(reader.ReadSingle());
+                //    }
+                //}
+            }
+        }
+    }
+}
---
+++ Area Game Files/Assets/TestFilesW/bepuMyResolution/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+//using UnityEngine.Experimental.UIElements;
+//using TMPro;
+
+
+namespace Dim
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public UnityEngine.UI.Button startBtn, networkChangeBtn, exitBtn;
+        public InputField ipAdresInputField;
+        public Text infoText;
+        public Text networkChangText;
+        public string lanDesc = "Lan", netDesc = "Net";
+        public DarkRift.Client.Unity.UnityClient client;
+        public string lanIPDesc = "127.0.0.1", netIPDesc = "127.0.0.1";
+        public NetworkPlayerDimManager npdm;
+        public GameObject MainMenuPanelGob;
+
+        bool lan = true;
+
+
+        void Start()
+        {
+            exitBtn.onClick.AddListener(EndGame);
+            networkChangeBtn.onClick.AddListener(ChangeNetworkArea);
+            startBtn.onClick.AddListener(StartGame);
+            LanOrNet();
+            infoText.text = "";
+            //ipAdresInputField.onValueChanged = ChangeLocalIp();
+            ipAdresInputField.onValueChanged.AddListener(delegate { ChangeLocalIp(ipAdresInputField.text); });
+            // client.error
+            client.Disconnected += Diconnect; 
+            
+        }
+
+        void Diconnect(object sender,DarkRift.Client.DisconnectedEventArgs arg)
+        {
+            EndGame2();
+        }
+
+        void ChangeLocalIp(string to)
+        {
+            lanIPDesc = to;
+            LanOrNet();
+        }
+
+
+        void ChangeNetworkArea()
+        {
+            lan = !lan;
+            LanOrNet();
+        }
+
+
+        void LanOrNet()
+        {
+            if (lan)
+            {
+                networkChangText.text = lanDesc;
+                //Client.ConnectInBackground(IPAddress.Parse(IpAdress), Port, IPVersion.IPv4, ConnectCallback);
+                client.Address = IPAddress.Parse(lanIPDesc);
+                ipAdresInputField.gameObject.SetActive(true) ;
+                ipAdresInputField.text = lanIPDesc;
+            }
+            else
+            {
+                networkChangText.text = netDesc;
+                client.Address = IPAddress.Parse(netIPDesc);
+                ipAdresInputField.gameObject.SetActive(false);
+                ipAdresInputField.text = netIPDesc;
+            }
+        }
+
+
+        void EndGame()
+        {
+            Application.Quit();
+        }
+
+
+        void StartGame()
+        {
+            client.Connect(client.Address, client.Port, client.IPVersion);
+           // client.
+            MainMenuPanelGob.SetActive(false);
+        }
+
+        void EndGame2()
+        {
+            //sprzątnij za sobą obiekty
+            MainMenuPanelGob.SetActive(true);
+            SetPositionFromServer[] sc = FindObjectsOfType<SetPositionFromServer>();
+            foreach (SetPositionFromServer s in sc)
+                Destroy(s.gameObject);
+            npdm.ClearAllPlayerDic();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                client.Disconnect();
+                EndGame2();
+            }
+        }
+    }
+}
