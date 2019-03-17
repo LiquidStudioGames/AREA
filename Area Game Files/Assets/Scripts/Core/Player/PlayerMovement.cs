193ad982ab823fb651321ca9316a90d90c0877df
Gonzako <al341522@uji.es>
2019-03-17 12:29:48 +0100

    unWIPed PlayerMovement

--- Area Game Files/Assets/Scripts/Core/Player/PlayerMovement.cs
+++ Area Game Files/Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementWIP : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField]
