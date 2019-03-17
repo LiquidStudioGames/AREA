e8c1348b9f349ab1687ff5596f4e3e0b9dadeb06
Paweł <pawlo168@gmail.com>
2019-03-17 07:11:05 +0100

    DarkRift new version support

---
+++ Assets/DarkRift/DarkRift/Plugins/Client/SerializableObjectCacheSettings.cs
@@ -0,0 +1,104 @@
+﻿using DarkRift;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///     Since <see cref="ObjectCacheSettings"/> uses properties, Unity can't serialize it to we clone it here and provide conversion methods.
+/// </summary>
+[Serializable]
+public sealed class SerializableObjectCacheSettings
+{
+    [Tooltip("")]
+    [SerializeField]
+    int maxWriters = 2;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxReaders = 2;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxMessages = 4;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxMessageBuffers = 4;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxSocketAsyncEventArgs = 32;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxActionDispatcherTasks = 16;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxAutoRecyclingArrays = 4;
+
+    [Tooltip("")]
+    [SerializeField]
+    int extraSmallMemoryBlockSize = 16;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxExtraSmallMemoryBlocks = 2;
+
+    [Tooltip("")]
+    [SerializeField]
+    int smallMemoryBlockSize = 64;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxSmallMemoryBlocks = 2;
+
+    [Tooltip("")]
+    [SerializeField]
+    int mediumMemoryBlockSize = 256;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxMediumMemoryBlocks = 2;
+
+    [Tooltip("")]
+    [SerializeField]
+    int largeMemoryBlockSize = 1024;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxLargeMemoryBlocks = 2;
+
+    [Tooltip("")]
+    [SerializeField]
+    int extraLargeMemoryBlockSize = 4096;
+
+    [Tooltip("")]
+    [SerializeField]
+    int maxExtraLargeMemoryBlocks = 2;
+
+    public ObjectCacheSettings ToObjectCacheSettings()
+    {
+        ObjectCacheSettings objectCacheSettings = new ObjectCacheSettings();
+        objectCacheSettings.MaxWriters = maxWriters;
+        objectCacheSettings.MaxReaders = maxReaders;
+        objectCacheSettings.MaxMessages = maxMessages;
+        objectCacheSettings.MaxMessageBuffers = maxMessageBuffers;
+        objectCacheSettings.MaxSocketAsyncEventArgs = maxSocketAsyncEventArgs;
+        objectCacheSettings.MaxActionDispatcherTasks = maxActionDispatcherTasks;
+        objectCacheSettings.MaxAutoRecyclingArrays = maxAutoRecyclingArrays;
+
+        objectCacheSettings.ExtraSmallMemoryBlockSize = extraSmallMemoryBlockSize;
+        objectCacheSettings.MaxExtraSmallMemoryBlocks = maxExtraSmallMemoryBlocks;
+        objectCacheSettings.SmallMemoryBlockSize = smallMemoryBlockSize;
+        objectCacheSettings.MaxSmallMemoryBlocks = maxSmallMemoryBlocks;
+        objectCacheSettings.MediumMemoryBlockSize = mediumMemoryBlockSize;
+        objectCacheSettings.MaxMediumMemoryBlocks = maxMediumMemoryBlocks;
+        objectCacheSettings.LargeMemoryBlockSize = largeMemoryBlockSize;
+        objectCacheSettings.MaxLargeMemoryBlocks = maxLargeMemoryBlocks;
+        objectCacheSettings.ExtraLargeMemoryBlockSize = extraLargeMemoryBlockSize;
+        objectCacheSettings.MaxExtraLargeMemoryBlocks = maxExtraLargeMemoryBlocks;
+        return objectCacheSettings;
+    }
+}
