6ca9de8f617a5f00f395d0cd95d073cc83fe03ed
Gonzako <al341522@uji.es>
2019-04-03 17:36:18 +0200

    Added simple Event system

--- Area Game Files/Assets/Scripts/Core/Shooting/AbsBullet.cs
+++ Area Game Files/Assets/Scripts/Core/Shooting/AbsBullet.cs
@@ -8,20 +8,30 @@ public abstract class AbsBullet : MonoBehaviour
     protected float muzzleVel; //velocity in m/s (1 unit = 1 meter)
     [SerializeField]
     protected int damage;
+    public LayerMask mask;
 
+
+    protected RaycastHit hit;
     protected Vector3 angle;
     protected Transform position;
-
+    
 
     public virtual void Awake()
     {
 
-
-        
     }
 
     public virtual void Update()
     {
         
     }
+
+    protected virtual void checkForward()
+    {
+        if(Physics.Raycast(position.position, angle, out hit, muzzleVel*Time.deltaTime, mask))
+        {
+            //Event system goes here
+        }
+    }
+
 }
